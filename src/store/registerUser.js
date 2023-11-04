aff4ce49684c11c819dd485d7822028ecf8b594c
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-11-04 10:49:30 +0530

    store the profile photopath to userData

--- src/store/registerUser.js
+++ src/store/registerUser.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes , getDownloadURL } from "firebase/storage";
 import { app, db } from '../firebase.js';
 import { reactive, toRefs } from 'vue';
 const auth = getAuth(app);
@@ -14,15 +14,19 @@ export const userRegisterUse = defineStore('userRegister', () => {
     const createUser = async (userData) => {
         try {
             const authResult = await createUserWithEmailAndPassword(auth, userData.email, userData.password);
+            const storageRef = ref(storage, `userProfile/${authResult.user.uid}/profilepic`);
+            uploadBytes(storageRef, userData.profilePhoto);
+
+            const downloadURL = await getDownloadURL(storageRef);
+
             const storedata = await addDoc(collection(db, "userDetails"), {
                 uid: authResult.user.uid,
                 firstName: userData.firstName,
                 lastName: userData.lastName,
                 mobileNumber: userData.mobileNumber,
-                email: userData.email
+                email: userData.email ,
+                profilePhotoPath: downloadURL
             })
-            const storageRef = ref(storage, `userProfile/${authResult.user.uid}/profilepic`);
-            uploadBytes(storageRef, userData.profilePhoto);
             state.existUserError = false;
             console.log(authResult, storedata)
         } catch (error) {
