d9b9bd7b2c6b0cb39e5703a96439b25cf02d6c07
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-11-17 16:58:07 +0530

    show all post to user

--- src/pages/showPost.vue
+++ src/pages/showPost.vue
@@ -1,15 +1,61 @@
 <template>
-    <div class="text-center">
-        <div>Sucessfull login</div>
-        <v-btn class="mt-2 pointer m-auto ml-2" color="blue" @click="logout">
-            logout
-        </v-btn>
+    <div class="container mx-auto max-w-3xl">
+        <div class="header text-right p-4">
+            <v-btn class="mt-2 pointer mr-2" color="blue" @click="logout">
+                Logout
+            </v-btn>
+        </div>
+
+        <div v-if="posts && posts.length" class=" flex flex-wrap gap-8 text-center">
+            <v-infinite-scroll>
+                <div v-for="post in posts" :key="post.id"
+                    class=" bg-white border w-4/5 border-gray-300 rounded-lg overflow-hidden shadow-md">
+                    <div class=" p-4 flex items-center">
+                        <img :src="post.userDetails.profilePhotoPath" alt="Profile Photo"
+                            class=" w-10 h-10 rounded-full mr-4" />
+                        <h2 class=" text-lg font-bold">{{ post.userDetails.firstName }} {{ post.userDetails.lastName }}</h2>
+                    </div>
+                    <h2 class=" text-center p-4 text-xl">{{ post.title }}</h2>
+                    <img :src="post.photo" alt="Post Photo" class=" w-5/5 m-auto object-cover" />
+                    <div @click="toggleTagUser(post.id)" class="text-left ml-3 mb-2 cursor-pointer ">
+                        <v-icon aria-hidden="false">
+                            mdi-account
+                        </v-icon>
+                    </div>
+                    <div v-if="tagUsers[post.id]">
+                        <div v-for="tagUser in post.taggedUserNames" :key="tagUser.id" class="text-left ml-3 mb-2">
+                            <h2>{{ tagUser }}</h2>
+                        </div>
+                    </div>
+                </div>
+            </v-infinite-scroll>
+        </div>
+        <div v-else class="text-center m-auto">
+            <v-progress-circular indeterminate></v-progress-circular>
+        </div>
     </div>
 </template>
 <script setup>
+import { ref, onMounted } from 'vue';
 import { useAuthUserStore } from '../store/auth-user-store.js';
+import { postStore } from '@/store/post-store';
+const authUser = useAuthUserStore();
+const { logout } = authUser;
+const userPosts = postStore()
+const { getAllPosts } = userPosts;
+const tagUsers = ref({});
+const posts = ref([]);
+const toggleTagUser = (postId) => {
+    tagUsers.value[postId] = !tagUsers.value[postId];
+};
+onMounted(async () => {
+    try {
+        posts.value = await getAllPosts();
 
-const store = useAuthUserStore();
-const { logout } = store;
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 </script>
--- src/store/post-store.js
+++ src/store/post-store.js
@@ -1,5 +1,5 @@
 import { ref, uploadBytes, getStorage, getDownloadURL } from "firebase/storage";
-import { collection, addDoc} from "firebase/firestore";
+import { collection, addDoc, getDocs, query, doc, where } from "firebase/firestore";
 import { db } from "../firebase.js";
 import slugify from 'slugify';
 import { useAuthUserStore } from './auth-user-store.js';
@@ -39,6 +39,65 @@ export const postStore = (() => {
         }
 
     };
-  
-    return { addPost };
+
+    const getAllPosts = async () => {
+        try {
+            let posts = []
+            const querySnapshot = await getDocs(query(collection(db, "post")));
+            if (!querySnapshot.empty) {
+                for (const doc of querySnapshot.docs) {
+                    const post = { ...doc.data(), id: doc.id };
+                    post.userDetails = await getUserDetails(post.updatedBy);
+                    post.taggedUserNames = await getTaggedUserNames(doc.id);
+                    posts.push(post);
+                }
+                return posts;
+            } else {
+                console.error("not found ");
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    const getTaggedUserNames = async (postId) => {
+        try {
+            const taggedUserRef = collection(db, `post/${postId}/taggedUsers`);
+            const querySnapshot = await getDocs(taggedUserRef);
+            if (!querySnapshot.empty) {
+                const userNames = [];
+                for (const doc of querySnapshot.docs) {
+                    const userIds = doc.data().userId;
+                    for (const userId of userIds) {
+                        const userDetails = await getUserDetails(userId);
+                        const userName = `${userDetails.firstName} ${userDetails.lastName}`;
+                        userNames.push(userName);
+                    }
+                }
+                return userNames;
+            } else {
+                console.error("No tagged users found for post ID:", postId);
+                return [];
+            }
+        } catch (error) {
+            console.error("Error fetching tagged user names:", error);
+            return [];
+        }
+    }
+    const getUserDetails = async (uid) => {
+        try {
+            const userDetailsCollection = collection(db, "userDetails");
+            const querySnapshot = await getDocs(query(userDetailsCollection, where("uid", "==", uid)));
+            if (!querySnapshot.empty) {
+                const userDoc = querySnapshot.docs[0].data();
+                const { profilePhotoPath, firstName, lastName } = userDoc;
+                return { profilePhotoPath, firstName, lastName };
+            } else {
+                console.error("User document not found for UID:", uid);
+            }
+        } catch (error) {
+            console.error("Error updating user details:", error);
+        }
+    };
+    return { addPost, getAllPosts, getUserDetails };
 });
